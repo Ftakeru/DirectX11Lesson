d26c7fad13883b4d8d3a35a2a4799a1181aa969e
FukudaTakeru <kd1281760@st.kobedenshi.ac.jp>
2021-04-21 11:37:25 +0900

    【授業プロジェクト】 ・いったん削除

---
+++ 2021_3DPro1/BaseFramework/Src/Application/Game/GameSystem.cpp
@@ -0,0 +1,24 @@
+﻿#include "GameSystem.h"
+
+//初期化
+void GameSystem::Init()
+{
+	m_sky.Load("Data/Models/Sky/Sky.gltf");
+}
+
+void GameSystem::Update()
+{
+	OutputDebugStringA("更新関数\n");
+}
+
+void GameSystem::Draw()
+{
+	SHADER->m_effectShader.SetToDevice();
+
+	SHADER->m_effectShader.DrawModel(m_sky);
+}
+
+void GameSystem::Release()
+{
+	OutputDebugStringA("解放関数\n");
+}
---
+++ 2021_3DPro1/BaseFramework/Src/Application/Game/GameSystem.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+class GameSystem
+{
+public:
+	//コンストラクター
+	GameSystem(){}
+	//デストラクター
+	~GameSystem() { Release(); }
+
+	void Init();	//初期化
+	void Update(); //更新
+	void Draw();	//描画
+
+private:
+	void Release();//解放
+	
+	KdModelWork m_sky; //空のモデル
+};
